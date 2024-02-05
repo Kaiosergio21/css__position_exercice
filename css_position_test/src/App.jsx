644c117eb627cf978520673089b9b213dab4e0ae
kaio sergio <kaiosergio21k@gmail.com>
2024-02-05 16:12:04 -0300

    first commit

---
+++ css_position_test/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Css_Position_Test from "./css_position_test";
+import'./css_position_test.css'
+
+function App(){
+
+
+
+return(
+<>
+<div>
+<Css_Position_Test></Css_Position_Test>
+</div>
+
+</>
+
+);
+
+
+}
+
+export default App;
---
+++ css_position_test/src/css_position_test.jsx
@@ -0,0 +1,182 @@
+import React from "react";
+
+
+
+
+
+function csspositiontest(){
+
+    
+    return(
+        <>
+
+<h1 className="grid">grid 1 - columms equali centered</h1>
+
+        <div className="grid_container">
+
+
+
+        <div className="black">
+                
+        <div className="content">
+                    <h2>Main Text 1</h2>
+                   <p>Subtext 1</p>
+                </div> 
+        
+
+        </div>
+
+        
+
+       
+
+        
+        <div className="red">
+                        
+        <div className="content">
+                    <h2>Main Text 1</h2>
+                   <p>Subtext 1</p>
+                </div> 
+        
+        
+             
+        
+        </div>
+
+
+
+
+        <div className="yellow">
+
+                        
+        <div className="content">
+                    <h2>Main Text 1</h2>
+                   <p>Subtext 1</p>
+                </div> 
+        
+
+        </div>
+        
+        
+
+                
+                
+        
+
+        </div>
+
+       
+<h1 className="grid">grid 2 - columms with diferent distantiation</h1>
+
+        <div className="grid_container2">
+
+
+            
+        <div className="black">
+                
+                <div className="content">
+                        
+                        </div> 
+                
+        
+                </div>
+        
+                
+        
+               
+        
+                
+                <div className="red">
+                                
+                <div className="content">
+                        
+                        </div> 
+                
+                
+                     
+                
+                </div>
+        
+        
+        
+        
+                <div className="yellow">
+        
+                                
+                <div className="content">
+                        
+                        </div> 
+                
+        
+                </div>
+                
+                
+        
+
+
+   </div>
+
+
+
+        <h1 className="grid">grid 3 - rows equali centered</h1>
+
+<div className="grid_container3">
+
+
+
+     <div className="black">
+
+     </div>
+
+
+         <div className="red">
+
+      </div>
+
+
+
+<div className="yellow">
+
+        </div>
+
+
+
+        </div>
+
+<h1 className="grid">grid 4 - rows equali centered</h1>
+
+   <div className="grid_container4">
+
+
+
+    <div className="black">
+
+    </div>
+
+
+       <div className="red">
+
+      </div>
+
+
+
+             <div className="yellow">
+
+             </div>
+
+
+
+            </div>
+
+
+
+
+    </>
+
+    );
+    
+    
+
+}
+
+export default csspositiontest;
